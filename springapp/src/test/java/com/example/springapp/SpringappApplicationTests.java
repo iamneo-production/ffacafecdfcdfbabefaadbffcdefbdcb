bcc2b28332ca4ef31f00c0a8845dbedc3573f851
Iamneo <techadmin@iamneo.ai>
2023-08-03 11:29:27 +0000

    Backup Commit

---
+++ springapp/src/test/java/com/example/springapp/SpringappApplicationTests.java
@@ -0,0 +1,88 @@
+package com.example.springapp;
+
+import org.springframework.http.MediaType;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class) 
+@SpringBootTest(classes = SpringappApplication.class)
+@AutoConfigureMockMvc
+class SpringappApplicationTests {
+
+	@Autowired
+	private  MockMvc mockMvc ;
+
+	@Test
+	void test_case1() throws Exception{	
+
+		String st = "{\"songId\":1000,\"musicDirectorName\": \"Demo\",\"yearOfRelease\": 1998,\"songName\": \"ÄBCD\",\"movieName\": \"XYZ\"}";
+		 mockMvc.perform(MockMvcRequestBuilders.post("/")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(st)
+					.accept(MediaType.APPLICATION_JSON))
+					.andExpect(MockMvcResultMatchers.status().isOk())
+					.andExpect(jsonPath("$").value(true))
+					.andReturn();
+	}
+
+	@Test
+	void test_case2() throws Exception{	
+
+		 mockMvc.perform(get("/1000")
+		 				.accept(MediaType.APPLICATION_JSON))
+						.andDo(print())
+						.andExpect(status().isOk())
+						.andExpect(jsonPath("$.musicDirectorName").value("Demo"))
+						.andReturn();
+	}
+
+	
+
+	@Test
+	void test_case3() throws Exception{	
+
+		String st = "{\"songId\":1000,\"musicDirectorName\": \"Demo1\",\"yearOfRelease\": 1999,\"songName\": \"ABCDE\",\"movieName\": \"XYZ1\"}";
+		mockMvc.perform(MockMvcRequestBuilders.put("/1000")
+					   .contentType(MediaType.APPLICATION_JSON)
+					   .content(st)
+					  .accept(MediaType.APPLICATION_JSON))
+					  .andExpect(MockMvcResultMatchers.status().isOk())
+					  .andExpect(jsonPath("$.yearOfRelease").value(1999))
+					  .andReturn();
+	}
+
+	@Test
+	void test_case4() throws Exception{	
+		
+		 mockMvc.perform(get("/")
+		 				.accept(MediaType.APPLICATION_JSON))
+						.andDo(print())
+						.andExpect(status().isOk())
+						.andExpect(jsonPath("$").isArray())
+						.andReturn();
+	}
+
+	@Test
+	void test_case5() throws Exception{	
+		mockMvc.perform(MockMvcRequestBuilders.delete("/1000")
+						.accept(MediaType.APPLICATION_JSON))
+						.andDo(print())
+						.andExpect(status().isOk())
+						.andReturn();
+	}
+
+}
